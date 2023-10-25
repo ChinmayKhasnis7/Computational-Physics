59c4bd1247d0601650425ae234d7d1abf6c4d2a0
Chinmay G Khasanis <chinmaykhasanis@gmail.com>
2023-10-25 22:29:43 +0530

    Create RK4_mod.py
    Modified Runge-Kutta 4th order

---
+++ RK4_mod.py
@@ -0,0 +1,24 @@
+def RK4_mod(X0, t0, tN, N, F):
+    """
+    X0      : X(t0)
+    [t0,tN] : Interval in which X(t) has to be found
+    N       : Number of solution points
+    Fun     : F(x) = dX/dt
+    Returns the solution X(t) for those points before hitting ground
+    """
+    dimension  = X0.shape[0]  # size of the phase space vector
+    q = 1-int(1/dimension)
+    X = np.zeros([dimension,N]) ; X[:,0]=X0  # Initialise the initial value
+    t = np.linspace(t0, tN, N)
+    h = (tN-t0)/N
+    for i in range(N-1):
+        K1 = h * F(t[i], X[:,i])
+        K2 = h * F(t[i] + 0.5 * h, X[:,i] + 0.5 * K1)
+        K3 = h * F(t[i] + 0.5 * h, X[:,i] + 0.5 * K2)
+        K4 = h * F(t[i] + h, X[:,i] + K3)
+        X[:,i+1] = X[:,i] + (K1 + 2 * K2 + 2 * K3 + K4) / 6
+        if X[q,i+1] <= 0:  # Stop when it hit ground
+            break
+    X = X[:,:i+1]
+    t = t[:i+1]
+    return X,t
